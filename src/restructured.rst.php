75236ce2022e8f59a8f019341272737ca368dc13
Octavio Alvarez <octalgl@alvarezp.org>
2019-06-02 10:09:01 -0500

    Add back support for reStructureText
    It's way more consistent than it previously was and
    hopefully we can get some testing this time.

---
+++ src/restructured.rst.php
@@ -0,0 +1,28 @@
+---
+title: Title set as yaml too!
+---
+
+=================================
+This is a reStructuredText sample
+=================================
+
+.. sectnum::
+
+.. contents:: The tiny table of contents
+
+Section 1
+~~~~~~~~~
+
+This is just to text if Pandoc supports RST lists:
+
+- lists
+- should
+- be
+- supported
+ 
+  - nested
+  - should
+  - be
+  - too
+
+
